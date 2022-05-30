67797a5bbc1769f616871c9974e91bcac83f7250
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-30 13:27:34 -0300

    Adding validations

--- src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -6,8 +6,8 @@ import javax.validation.constraints.Size
 
 data class NovoTopicoForm (
 
-    @NotEmpty @Size(min = 5, max = 100) val titulo: String,
-    @NotEmpty val mensagem: String,
-    @NotNull val idCurso: Long,
-    @NotNull val idAutor: Long
+    @field:NotEmpty @Size(min = 5, max = 100) val titulo: String,
+    @field:NotEmpty val mensagem: String,
+    @field:NotNull val idCurso: Long,
+    @field:NotNull val idAutor: Long
         )
