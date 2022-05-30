e92b70db9c5617247eef72df03207844c96b5189
VictorArruda22 <victor.ti.henrique@gmail.com>
2022-05-30 17:59:07 -0300

    Adding return 201, 204

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -30,18 +30,19 @@ class TopicoService(private var topicos: kotlin.collections.List<Topico> = Array
         return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(form: NovoTopicoForm) {
+    fun cadastrar(form: NovoTopicoForm): TopicoView {
        val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+
+        return topicoViewMapper.map(topico)
     }
 
-    fun atualizar(form: AtualizacaoTopicoForm) {
+    fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
         val topico = topicos.stream().filter { t ->
             t.id == form.id
         }.findFirst().get()
-        topicos = topicos.minus(topico).plus(
-            Topico(
+        val topicoAtualizado = Topico(
             id = form.id,
             titulo = form.titulo,
             mensagem = form.mensagem,
@@ -50,7 +51,9 @@ class TopicoService(private var topicos: kotlin.collections.List<Topico> = Array
             curso = topico.curso,
             respostas = topico.respostas,
             status = topico.status
-        ))
+        )
+        topicos = topicos.minus(topico).plus(topicoAtualizado)
+        return topicoViewMapper.map(topicoAtualizado)
     }
 
     fun deletar(id: Long) {
